b106a7dabfe7b9a598781abab7fde91c4d57ee58
Mostafa Elshazly <mostafaelshazly@mostafas-macbook-air.local>
2018-09-13 21:02:56 +0200

    Initial Commit

---
+++ Todos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todos
+//
+//  Created by Mostafa Elshazly on 9/13/18.
+//  Copyright © 2018 Mostafa Elshazly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
