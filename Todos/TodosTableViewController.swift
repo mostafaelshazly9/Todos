73fd7e3c6b56c2078a8d7fbf56787ea9e7dac14e
Mostafa Elshazly <mostafaelshazly@mostafas-macbook-air.local>
2018-09-14 15:10:53 +0200

    Todo list with datasource and delegate methods

---
+++ Todos/TodosTableViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Todos
+//
+//  Created by Mostafa Elshazly on 9/13/18.
+//  Copyright © 2018 Mostafa Elshazly. All rights reserved.
+//
+
+import UIKit
+
+class TodosTableViewController: UITableViewController {
+
+    var todosItems = ["Read a book", "Clean room", "Make a routine"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK - TableView datasource methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todosItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodosCell", for: indexPath)
+        cell.textLabel?.text = todosItems[indexPath.row]
+        return cell
+    }
+    
+    //MARK - Delegate methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    
+    
+}
+
